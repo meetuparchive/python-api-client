99e97d82e0d036ac1919999abbe4c23e0955321f
Nathan Hamblen <nathan@technically.us>
2009-11-26 09:22:01 -0500

    some additions to the Event interface

--- meetup_api_client.py
+++ meetup_api_client.py
@@ -246,7 +246,7 @@ class Photo(API_Item):
 
 
 class Event(API_Item):
-    datafields = ['id', 'name', 'updated', 'time', 'photo_url', 'event_url']
+    datafields = ['id', 'name', 'updated', 'time', 'photo_url', 'event_url', 'venue_lat', 'venue_lon', 'description']
 
     def __str__(self):
         return 'Event %s named %s at %s (url: %s)' % (self.id, self.name, self.time, self.event_url)
