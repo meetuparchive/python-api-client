9bb37af54cdfe207afeecdfae3e8968281e4b781
shaisachs <ssachs@ngpvan.com>
2016-07-15 15:10:48 -0400

    Add support for find/groups and create a client to demonstrate apikey-style requests
    Signed-off-by: shaisachs <ssachs@ngpvan.com>

--- meetup_api_client.py
+++ meetup_api_client.py
@@ -43,6 +43,7 @@ RSVP_URI = 'rsvp'
 COMMENTS_URI = 'comments'
 PHOTO_URI = 'photo'
 MEMBER_PHOTO_URI = '2/member_photo'
+FIND_GROUPS_URI = 'find/groups'
 
 API_BASE_URL = 'http://api.meetup.com/'
 OAUTH_BASE_URL = 'http://www.meetup.com/'
@@ -89,6 +90,9 @@ class Meetup(object):
     def post_member_photo(self, **args):
         return self._post_multipart(MEMBER_PHOTO_URI, **args)
 
+    def find_groups(self, **args):
+        return self._fetch(FIND_GROUPS_URI, **args)
+
     def args_str(self, url_args):
         if self.api_key:
             url_args['key'] = self.api_key
@@ -97,6 +101,7 @@ class Meetup(object):
     def _fetch(self, uri, **url_args):
         args = self.args_str(url_args)
         url = API_BASE_URL + uri + '/' + "?" + args
+
         logging.debug("requesting %s" % (url))
         return parse_json(self.opener.open(url).read())
 
